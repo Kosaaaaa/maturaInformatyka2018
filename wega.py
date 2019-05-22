6d04ef1763ee416f984fa0fdfaf703708d6fa404
Kosaaaaa <oskarkosobucki@gmail.com>
2019-05-22 16:13:54 +0200

    add  function zad42

--- wega.py
+++ wega.py
@@ -8,5 +8,27 @@ def zad41():
                 print(linia[9],end='')
             i+=1
         
+def zad42():
+    with open('Dane_PR2/przyklad.txt','r') as plik2:
+    #Ilosc - zmienna odpowiadajaca za ilosc lini do wczytania
+        ilosc=1000
+        maks=0
+        slowo=""
+    
+        for i in range(ilosc):
+            znaki=[]
+            linia = plik2.readline()
+            #wpisywanie znakow z lini do listy
+            for znak in linia:
+                if not znak in znaki:
+                    znaki.append(znak)
+                if '\n' in znaki:
+                    znaki.remove('\n')
+            znaki.sort()
+            if len(znaki)>maks:
+                maks=len(znaki)
+                slowo=linia
+            
+    
+    print(slowo + str(maks))
 
-zad41()
